29cf415184b11e772eadf027720e7abc1bc26cd8
Yves Gurcan <gurcan.yves+github@gmail.com>
2023-06-30 21:03:05 -0700

    FIrst build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import process from 'node:process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: '../',
+    },
+    root: 'src',
+    resolve: {
+        alias: { '/src': path.resolve(process.cwd(), 'src') },
+    },
+});
